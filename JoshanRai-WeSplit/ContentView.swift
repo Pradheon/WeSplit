1820b4b19e561f8c0b19747faf57cc0fc170cf01
Pradheon <pradheon@gmail.com>
2022-01-26 15:39:46 -0800

    Initial Commit

---
+++ JoshanRai-WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JoshanRai-WeSplit
+//
+//  Created by Joshan Rai on 1/26/22.
+//
+//  This app made following the "100 Days of SwiftUI" curiculum by Paul Hudson at https://www.hackingwithswift.com/100/swiftui
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshanRai-WeSplit/JoshanRai_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_WeSplitApp.swift
+//  JoshanRai-WeSplit
+//
+//  Created by Joshan Rai on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
