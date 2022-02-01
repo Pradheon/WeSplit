a7dbbf465c050aff1a0ceeb6b0045158f2f7f5e3
Pradheon <pradheon@gmail.com>
2022-01-31 19:14:46 -0800

    Added: - App Icons for iOS, watchOS, macOS, and CarPlay - Form view & picker view - UI input for check amount with localization - UI picker for number of people - Title to form view

--- JoshanRai-WeSplit/ContentView.swift
+++ JoshanRai-WeSplit/ContentView.swift
@@ -11,9 +11,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var nPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }.navigationTitle("WeSplit")
+                
+                Picker("Number of people", selection: $nPeople) {
+                    ForEach(2..<100) {
+                        Text("\($0) people")
+                    }
+                }
+            }
+        }
     }
 }
 
